792acd3dc8d1cf47ef67f87bfcf65cd6564a889a
DariaKalash <kalasnikova.dg@gmail.com>
2024-11-27 23:27:33 +0300

    woooow commit

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ win = bld.get_object('wnd')
 
 res = bld.get_object('res')
 
+inpa = bld.get_object('inp_a')
+inpb = bld.get_object('inp_b')
+inpc = bld.get_object('inp_c')
+
 def check(obj):
 	try:
 		a = int(bld.get_object('inp_a').get_text())
@@ -29,19 +33,19 @@ def check(obj):
 			elif a == b or b == c or a == c:
 				res.set_text('Треугольник равнобедренный')
 				return
-			elif a**2 + b**2 == c**2:
-				res.set_text('Треугольник прямоугольный')
-				return
-			elif a**2 + b**2 < c**2:
-				res.set_text('Треугольник тупоугольный')
-				return
 			else:
-				res.set_text('Треугольник остроугольный')
+				res.set_text('Треугольник разносторонний')
 				return
+
+def cls_vals(obj):
+	inpa.set_text('')
+	inpb.set_text('')
+	inpc.set_text('')
 
 tab={
 	"check_click": check,
-	"onDestroy": Gtk.main_quit
+	"onDestroy": Gtk.main_quit,
+	"clear_inp": cls_vals
 }
 
 bld.connect_signals(tab)
